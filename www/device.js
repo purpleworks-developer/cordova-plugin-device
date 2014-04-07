62daa7f6d5a9ec54bd36e40c24d3d3bcebc3d26f
ilyoung <ilyoung@ilyoungui-mbp.purple.lan>
2014-04-07 18:13:25 +0900

    update device.js

--- www/device.js
+++ www/device.js
@@ -41,6 +41,7 @@ function Device() {
     this.uuid = null;
     this.cordova = null;
     this.model = null;
+    this.pushToken = null;
 
     var me = this;
 
@@ -55,6 +56,7 @@ function Device() {
             me.uuid = info.uuid;
             me.cordova = buildLabel;
             me.model = info.model;
+            me.pushToken = info.pushToken;
             channel.onCordovaInfoReady.fire();
         },function(e) {
             me.available = false;
